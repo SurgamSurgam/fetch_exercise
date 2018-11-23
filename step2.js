c9c488c1c40330fde27cd9eda90906b794bda652
Deyvi <deyvio@gmail.com>
2018-11-23 17:35:16 -0500

    final version

--- step2.js
+++ step2.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let body = document.querySelector('body');
   let select = document.querySelector('select');
   let ul = document.querySelector('ul');
+  let arrayObjAllMovies = [];
 
 
   fetch('https://ghibliapi.herokuapp.com/films')
@@ -9,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
       return response.json();
     })
     .then(response => {
+      arrayObjAllMovies = response;
       showMovies(response)
     })
 
@@ -17,8 +19,45 @@ document.addEventListener("DOMContentLoaded", () => {
       let option = document.createElement('option');
       select.appendChild(option);
       option.innerText = el.title;
+      option.setAttribute('value', el.title);
     })
   }
+
+// // Outside of function so li gets redefined;
+let title = document.createElement('li');
+let director = document.createElement('li');
+let description = document.createElement('li');
+let releaseDate = document.createElement('li');
+let rtScore = document.createElement('li');
+let h3 = document.createElement('h3');
+
+  function showDetails(movieName) {
+    arrayObjAllMovies.forEach(el => {
+      if (movieName === el.title) {
+        // debugger
+
+        title.innerHTML = `<strong>Title:</strong> ${el.title}`;
+        ul.appendChild(title);
+
+        director.innerHTML = `<strong>Director:</strong> ${el.director}`;
+        ul.appendChild(director);
+
+        description.innerHTML = `<strong>Description:</strong> <br>${el.description}`;
+        ul.appendChild(description);
+
+        releaseDate.innerHTML = `<strong>Release Date:</strong> ${el['release_date']}`;
+        ul.appendChild(releaseDate);
+
+        rtScore.innerHTML = `<strong>Rotten Tomato Score:</strong> ${el['rt_score']}`;
+        ul.appendChild(rtScore);
+      }
+    })
+
+  }
+
+  select.addEventListener('change', (event)=> {
+    showDetails(event.target.value);
+  })
 
 
 })
