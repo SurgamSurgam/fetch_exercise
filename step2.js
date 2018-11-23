54f27754af4e0ab6641f8fc52dcbd444648dd76e
Deyvi <deyvio@gmail.com>
2018-11-23 15:11:26 -0500

    added dropdown

--- step2.js
+++ step2.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   let body = document.querySelector('body');
+  let select = document.querySelector('select');
+  let ul = document.querySelector('ul');
 
-  
+
+  fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      showMovies(response)
+    })
+
+  function showMovies(arrayObj) {
+    arrayObj.forEach(el => {
+      let option = document.createElement('option');
+      select.appendChild(option);
+      option.innerText = el.title;
+    })
+  }
+
 
 })
